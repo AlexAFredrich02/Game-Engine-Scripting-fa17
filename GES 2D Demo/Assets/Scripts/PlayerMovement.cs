1130e7f7e9bef79adebd023fad4f821c8ca2d1d2
Alexandra Fredrich <Alexandra.Fredrich@iam.local>
2017-09-18 15:18:58 -0500

    Followed Along within Class demo

---
+++ GES 2D Demo/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //Debug.Log("Hello. This code is inside of Start.");
+
+        //This is code to teleport the player to a new position
+        //transform.position = new Vector3(0, 0, 0);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float speed = 0.2f;
+
+        transform.Translate(speed * horizontalInput, 0, 0);
+      
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            transform.Translate(0, 1, 0);
+        }
+
+	}
+}
